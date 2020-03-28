4113c261c1e80986db64ed431481f614bbab400b
pjm349@drexel.edu <pjm349@drexel.edu>
2020-03-27 23:56:40 -0400

    Waiting for VS to finish updating... Got rudimentary rewrite in CSharp going. LAck of intellisense is disconcerting

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # Dodge the Creeps
-"Dodge the Creeps!"  Godot Engine 3.0 "getting started" game demonstrating the basics.  
 
+"Dodge the Creeps!"  Godot Engine 3.0 "getting started" game demonstrating the basics.  
 This game is a companion to the Godot Engine 3.0 Getting Started Guide: http://docs.godotengine.org/en/latest/getting_started/step_by_step/your_first_game.html  
-It is a game intended to demonstrate the following fundamental concepts of Godot development:  
+
+## It is a game intended to demonstrate the following fundamental concepts of Godot development:  
  - Node and scene structure
  - Instancing scenes
  - Input
  - Signals
  - UI
 
-I have tried doing this in both:  
+## I have tried doing this in both:  
  - GDScript (godot)
  - C# (mono)
+
+
+### Notes
+ - https://docs.godotengine.org/en/stable/getting_started/scripting/c_sharp/c_sharp_basics.html
+ - https://docs.godotengine.org/en/stable/development/compiling/compiling_with_mono.html#doc-compiling-with-mono
 
--- mono/script/Player.cs
+++ mono/script/Player.cs
@@ -1,21 +1,177 @@
+
+
+
+
 using Godot;
 using System;
 
+
+
+
+/// <summary>
+/// A Simple Player Script for Dodging Projectiles in 2D
+/// </summary>
 public class Player : Area2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
 
-	// Called when the node enters the scene tree for the first time.
+
+
+	#region variables
+	// Death
+	[Signal]
+	public delegate void Hit();								// Tell Enemies, Game, player died
+	// Movement
+    [Export]
+	public int speed = 400; 								// How fast the player will move (pixels/sec).
+    private Vector2 _screenSize; 							// Size of the game window.
+	private Position _moveTarget;							// Where we move towards
+	// Sprite
+	private const String _spriteName = "Sprite";			// What we call the sprite
+	private AnimatedSprite _sprite;							// What gets rendered to screen
+	// Hitbox
+	private const String _hitBoxName = "Hitbox";			// What we call the hitbox
+	private CollisionShape2d _hitBox;						// What collides with enemies
+	#endregion
+
+
+
+	#region lifecycles
+	/// <summary>
+	/// Prevent locked aspect ratio, async res loading
+	/// </summary>
 	public override void _Ready()
 	{
-		Console.WriteLine("HELLO");
+		// Console.WriteLine("HELLO");
+		SetScreenSize();
+		SetSprite();
+		SetHitBox();
+		// Hide();
+		// _moveTarget = self.Position;
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	public override void _Process(float delta)
+	{
+		ReadInput(delta);
+	}
+	#endregion
+
+
+
+
+	#region ready helpers
+	/// <summary>
+	/// Sets the `screen_size` variable
+	/// </summary>
+	private void SetScreenSize()
+	{
+		_screenSize = GetViewport().Size;
+	}
+
+	/// <summary>
+	/// Sets the player's sprite and configures it
+	/// </summary>
+	private void SetSprite()
+	{
+		_sprite = GetNode<AnimatedSprite>(_spriteName);
+	}
+
+	/// <summary>
+	/// Sets the player's collider and configures it
+	/// </summary>
+	private void SetHitBox()
+	{
+		_hitBox = self.GetNode<CollisionShape2d>(_hitBoxName);
+		_hitBox.SetDeferred("disabled", true);
+	}
+	#endregion
+
+
+
+
+	// INPUT HELPERS
+	/// <summary>
+	/// Decodes the player keypresses and trigger the corresponding events
+	/// </summary>
+	private void ReadInput(float delta)
+	{
+		// Get Input Direction
+		var velocity = new Vector2(); // The player's movement vector.
+
+		if (Input.IsActionPressed("ui_right"))
+		{
+			velocity.x += 1;
+		}
+
+		if (Input.IsActionPressed("ui_left"))
+		{
+			velocity.x -= 1;
+		}
+
+		if (Input.IsActionPressed("ui_down"))
+		{
+			velocity.y += 1;
+		}
+
+		if (Input.IsActionPressed("ui_up"))
+		{
+			velocity.y -= 1;
+		}
+
+				// Activate the Sprite
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * speed;
+			_sprite.Play();
+		}
+		else
+		{
+			_sprite.Stop();
+		}
+
+
+		// Move the Character
+		Position += velocity * delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.x, 0, _screenSize.x),
+			y: Mathf.Clamp(Position.y, 0, _screenSize.y)
+		);
+
+
+		// Choose the Animation
+		if (velocity.x != 0)
+		{
+			_sprite.Animation = "right";
+			_sprite.FlipV = false;
+			_sprite.FlipH = velocity.x < 0;
+		}
+		else if (velocity.y != 0)
+		{
+			_sprite.Animation = "up";
+			_sprite.FlipV = velocity.y > 0;
+		}
+	}
+
+
+
+
+	// INPUT EVENTS
+
+
+
+
+	// OUTPUT HELPERS
+
+
+
+
+	// ANIMATION HELPERS
+
+
+
+
+	// GAMEPLAY HELPERS
+
+
+
+
 }
